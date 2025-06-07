1bdcd83ba509d905f28abd93ae57033d619e4d68
Seah Chen Khoon <chenkhoon@yahoo.com>
2025-06-07 16:49:58 +0800

    refactor: Update cls_Lab_Result to support classification fields and rename datetime to test_date

--- src/utils/cls_Lab_Result.py
+++ src/utils/cls_Lab_Result.py
@@ -5,38 +5,41 @@ class cls_Lab_Result:
     def __init__(
         self,
         test_filename: Optional[str] = None,
-        test_datetime: Optional[datetime] = None,
+        test_date: Optional[datetime] = None,
         test_name: Optional[str] = None,
         test_result: Optional[float] = None,
         test_uom: Optional[str] = None,
-        ref_range: Optional[str] = None,
-        diagnostic: Optional[str] = None,
+        classification: Optional[str] = None,
+        reason: Optional[str] = None,
+        recommendation: Optional[str] = None,
     ):
         self.test_filename = test_filename
-        self.test_datetime = test_datetime
+        self.test_date = test_date
         self.test_name = test_name
         self.test_result = test_result
         self.test_uom = test_uom
-        self.ref_range = ref_range
-        self.diagnostic = diagnostic
+        self.classification = classification
+        self.reason = reason
+        self.recommendation = recommendation
 
     @classmethod
     def from_dict(cls, data: dict, filename:Optional[str]=None,
-                  test_datetime: Optional[str] = None) -> "cls_Lab_Result":
+                  test_date: Optional[str] = None) -> "cls_Lab_Result":
         return cls(
             test_filename= filename,
-            test_datetime= test_datetime or data.get("datetime"),
+            test_date= test_date or data.get("test_date"),
             test_name=data.get("test_name"),
             test_result=data.get("test_result"),
             test_uom=data.get("test_uom"),
-            ref_range=data.get("ref_range"),
-            diagnostic=data.get("diagnostic"),
+            classification=data.get("classification"),
+            reason=data.get("reason"),
+            recommendation=data.get("recommendation"),
         )
 
     def get_datetime_object(self) -> Optional[datetime]:
-        if self.test_datetime:
+        if self.test_date:
             try:
-                return datetime.strptime(self.test_datetime, "%d %b %Y, %I:%M %p")
+                return datetime.strptime(self.test_date, "%d %b %Y, %I:%M %p").date()
             except ValueError:
                 pass
         return None
