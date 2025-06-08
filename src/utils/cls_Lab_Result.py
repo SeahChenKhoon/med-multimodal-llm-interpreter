6c94ed24a6d2b4c3033cb7e2ec6176b0489feddb
Seah Chen Khoon <chenkhoon@yahoo.com>
2025-06-08 09:27:12 +0800

    feat: Add test_common_name and cls_Lab_Result_List container to lab result model

--- src/utils/cls_Lab_Result.py
+++ src/utils/cls_Lab_Result.py
@@ -1,11 +1,12 @@
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
 
 class cls_Lab_Result:
     def __init__(
         self,
         test_filename: Optional[str] = None,
         test_date: Optional[datetime] = None,
+        test_common_name: Optional[str] = None,
         test_name: Optional[str] = None,
         test_result: Optional[float] = None,
         test_uom: Optional[str] = None,
@@ -15,6 +16,7 @@ class cls_Lab_Result:
     ):
         self.test_filename = test_filename
         self.test_date = test_date
+        self.test_common_name = test_common_name
         self.test_name = test_name
         self.test_result = test_result
         self.test_uom = test_uom
@@ -28,6 +30,7 @@ class cls_Lab_Result:
         return cls(
             test_filename= filename,
             test_date= test_date or data.get("test_date"),
+            test_common_name=data.get("test_comon_name"),
             test_name=data.get("test_name"),
             test_result=data.get("test_result"),
             test_uom=data.get("test_uom"),
@@ -43,3 +46,8 @@ class cls_Lab_Result:
             except ValueError:
                 pass
         return None
+    
+class cls_Lab_Result_List():
+    def __init__(self):
+        self.result: List[cls_Lab_Result] = []
+
